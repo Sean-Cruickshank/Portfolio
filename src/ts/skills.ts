58c902664aeee9cb0c482512637bd405f84a9638
Sean Cruickshank <seancruickshank2023@gmail.com>
2025-09-02 23:38:45 +1200

    Added gallery for skills page

--- src/ts/skills.ts
+++ src/ts/skills.ts
@@ -26,6 +26,7 @@ const miscSkills = [
   { name: "Python", icon: "devicon-python-plain"},
 ]
 
+// Populates the rows of skills icons
 function handleUI(arr: skillArray[]) {
     let skillsListHTML = ''
 
@@ -47,3 +48,55 @@ if (javascript) javascript.innerHTML = handleUI(javascriptSkills)
 
 const misc = document.querySelector('.skills__misc')
 if (misc) misc.innerHTML = handleUI(miscSkills)
+
+const detailsArray = [
+  {
+    subtitle: 'Styling',
+    description: [
+      'This is a test of the description',
+      'Hopefully this will display correctly on the styling page'
+    ]
+  },
+  {
+    subtitle: 'JavaScript',
+    description: [
+      'This is a test of the description',
+      'Hopefully this will display correctly on the javascript page'
+    ]
+  },
+  {
+    subtitle: 'Misc',
+    description: [
+      'This is a test of the description',
+      'Hopefully this will display correctly on the misc page'
+    ]
+  },
+]
+
+// Adds functionality for the back and forward buttons
+const backButton = document.querySelector('.skills__back-button')
+backButton?.addEventListener('click', () => handleDetails('-'))
+
+const forwardButton = document.querySelector('.skills__forward-button')
+forwardButton?.addEventListener('click', () => handleDetails('+'))
+
+// 
+let position = 0
+function handleDetails(val: '+' | '-' | 0) {
+  if (val === '+') position < detailsArray.length - 1 ? position++ : position = 0
+  if (val === '-') position > 0 ? position-- : position = detailsArray.length - 1
+  if (val === 0) position === 0
+
+  const subtitle = document.querySelector('.skills__subtitle h3')
+  if (subtitle) subtitle.innerHTML = detailsArray[position].subtitle
+
+  let descriptionHTML = ``
+  detailsArray[position].description.forEach(paragraph => {
+    descriptionHTML += `<p>${paragraph}</p>`
+  })
+
+  const description = document.querySelector('.skills__description')
+  if (description) description.innerHTML = descriptionHTML
+}
+
+handleDetails(0)
