0f38061b2291280811d64a399d7d7db7e6c0767a
Sean Cruickshank <seancruickshank2023@gmail.com>
2025-09-04 00:15:58 +1200

    Worked on skills section logic

--- src/ts/skills.ts
+++ src/ts/skills.ts
@@ -3,57 +3,112 @@ type skillArray = {
     icon: string
 }
 
-const styleSkills = [
-  { name: "HTML", icon: "devicon-html5-plain-wordmark"},
-  { name: "CSS", icon: "devicon-css3-plain-wordmark"},
-  { name: "Bootstrap", icon: "devicon-bootstrap-plain"},
-  { name: "SASS", icon: "devicon-sass-original"},
-  { name: "Tailwind", icon: "devicon-tailwindcss-original"},
-]
-
-const javascriptSkills = [
-  { name: "JavaScript", icon: "devicon-javascript-plain"},
-  { name: "TypeScript", icon: "devicon-typescript-plain"},
-  { name: "React", icon: "devicon-react-original"},
-  { name: "NextJS", icon: "devicon-nextjs-original-wordmark"},
-]
-
-const miscSkills = [
-  { name: ".NET Core", icon: "devicon-dotnetcore-plain"},
-  { name: "SQL", icon: "devicon-azuresqldatabase-plain"},
-  { name: "Python", icon: "devicon-python-plain"},
-]
-
-const cloudSkills = [
-  { name: "Azure", icon: "devicon-azure-plain"},
-  { name: "Git", icon: "devicon-git-plain"},
+const data = [
+  {
+    title: 'styling',
+    description: 'I think I do pretty good at styling to be honest',
+    array: [
+      { name: "HTML", icon: "devicon-html5-plain-wordmark"},
+      { name: "CSS", icon: "devicon-css3-plain-wordmark"},
+      { name: "Bootstrap", icon: "devicon-bootstrap-plain"},
+      { name: "SASS", icon: "devicon-sass-original"},
+      { name: "Tailwind", icon: "devicon-tailwindcss-original"},
+    ]
+  },
+  {
+    title: 'javascript',
+    description: '',
+    array: [
+      { name: "JavaScript", icon: "devicon-javascript-plain"},
+      { name: "TypeScript", icon: "devicon-typescript-plain"},
+      { name: "React", icon: "devicon-react-original"},
+      { name: "NextJS", icon: "devicon-nextjs-original-wordmark"},
+    ]
+  },
+  {
+    title: 'misc',
+    description: '',
+    array: [
+      { name: ".NET Core", icon: "devicon-dotnetcore-plain"},
+      { name: "SQL", icon: "devicon-azuresqldatabase-plain"},
+      { name: "Python", icon: "devicon-python-plain"},
+    ]
+  },
+  {
+    title: 'cloud',
+    description: '',
+    array: [
+      { name: "Azure", icon: "devicon-azure-plain"},
+      { name: "Git", icon: "devicon-git-plain"},
+    ]
+  }
 ]
 
 // Populates the rows of skills icons
 function handleUI(arr: skillArray[]) {
     let skillsListHTML = ''
-    let count = 1
 
     arr.forEach(skill => {
         skillsListHTML+= `
-            <div class="skills__icon">
+            <div class="skills__icon hidden__fade">
                 <i class="${skill.icon}"></i>
                 <p>${skill.name}</p>
             </div>
         `
-        count++
     })
     return skillsListHTML
 }
 
-const styling = document.querySelector('.skills__styling')
-if (styling) styling.innerHTML = handleUI(styleSkills)
+let skillsHTML = ''
 
-const javascript = document.querySelector('.skills__js')
-if (javascript) javascript.innerHTML = handleUI(javascriptSkills)
+data.forEach(category => {
+  skillsHTML+= `
+    <div class='skills__${category.title} skills__list'>
+      ${handleUI(category.array)}
+    </div>
+  `
+})
 
-const misc = document.querySelector('.skills__misc')
-if (misc) misc.innerHTML = handleUI(miscSkills)
+const skills = document.querySelector('.skills')
+if (skills) skills.innerHTML = skillsHTML
 
-const cloud = document.querySelector('.skills__cloud')
-if (cloud) cloud.innerHTML = handleUI(cloudSkills)
+function renderSkills(category: string) {
+  const iconArray = document.querySelectorAll(category)
+  console.log(iconArray)
+  let delay = 0
+  iconArray.forEach(icon => {
+      setTimeout(() => {
+          icon.classList.remove('hidden__fade')
+      }, delay)
+      delay+= 350
+  })
+}
+
+let stylingThreshold = false
+let javascriptThreshold = false
+let miscThreshold = false
+let cloudThreshold = false
+
+window.addEventListener('scroll', () => {
+  const position = Math.floor(scrollY)
+  console.log(position)
+  if (position > 300 && !stylingThreshold) {
+    renderSkills('.skills__styling .skills__icon')
+    stylingThreshold = true
+  }
+
+  if (position > 500 && !javascriptThreshold) {
+  renderSkills('.skills__javascript .skills__icon')
+  javascriptThreshold = true
+  }
+
+  if (position > 700 && !miscThreshold) {
+  renderSkills('.skills__misc .skills__icon')
+  miscThreshold = true
+  }
+
+  if (position > 900 && !cloudThreshold) {
+  renderSkills('.skills__cloud .skills__icon')
+  cloudThreshold = true
+  }
+})
