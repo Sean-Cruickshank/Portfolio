5f44de8d828f87fceafdec56d8374fb668f7ee3c
Sean Cruickshank <seancruickshank2023@gmail.com>
2025-11-06 13:26:16 +1300

    Reworked skills section again

--- src/ts/skills.ts
+++ src/ts/skills.ts
@@ -4,22 +4,8 @@ type skillArray = {
 }
 
 const data = [
-  {
-    title: 'styling',
-    description: [
-      'I have spent a lot of time styling with CSS and feel confident with the common extensions and frameworks',
-      'I typically aim for a mobile-first approach and strive to create applications that are dynamic and responsive'
-    ],
-    render: { flag: false, breakpoint: 1000 },
-    array: [
-      { name: "HTML", icon: "devicon-html5-plain-wordmark"},
-      { name: "CSS", icon: "devicon-css3-plain-wordmark"},
-      { name: "Bootstrap", icon: "devicon-bootstrap-plain"},
-      { name: "SASS", icon: "devicon-sass-original"},
-      { name: "Tailwind", icon: "devicon-tailwindcss-original"},
-    ]
-  },
-  {
+    {
+    id: 0,
     title: 'javascript',
     description: [
       "JavaScript is probably the language I am the most familiar with, though these days I tend to use TypeScript by default",
@@ -35,6 +21,22 @@ const data = [
     ]
   },
   {
+    id: 1,
+    title: 'styling',
+    description: [
+      'I have spent a lot of time styling with CSS and feel confident with the common extensions and frameworks',
+      'I typically aim for a mobile-first approach and strive to create applications that are dynamic and responsive'
+    ],
+    render: { flag: false, breakpoint: 1000 },
+    array: [
+      { name: "CSS", icon: "devicon-css3-plain-wordmark"},
+      { name: "Bootstrap", icon: "devicon-bootstrap-plain"},
+      { name: "SASS", icon: "devicon-sass-original"},
+      { name: "Tailwind", icon: "devicon-tailwindcss-original"},
+    ]
+  },
+  {
+    id: 2,
     title: 'misc',
     description: [
       'I spent a lot of time working with C# and .NET during my degree, and I have plans to expand on this in the near future',
@@ -50,6 +52,7 @@ const data = [
     ]
   },
   {
+    id: 3,
     title: 'cloud',
     description: [
       'I completed my Azure Fundamentals certification in March 2025 and hope to delve deeper into the Azure pathway in the future',
@@ -63,59 +66,118 @@ const data = [
   },
 ]
 
-// Creates the div for each category and populates it, inserts all divs into the top-level 'skills' div
-let skillsHTML = ''
-data.forEach(category => {
-  let descriptionHTML = ''
-  category.description.forEach(paragraph => {
-    descriptionHTML+= `<p>${paragraph}</p>`
-  })
+let menuHTML = ''
 
-  skillsHTML+= `
-    <div>
-      <div class='skills__${category.title} skills__list'>
-        ${handleUI(category.array)}
-      </div>
-      ${descriptionHTML}
+// Generates the buttons for each category
+data.forEach(category => {
+  menuHTML += `
+    <div class='skills__menu__item'>
+      <button class='skills__menu__button skills__menu__${category.title}'>
+        <div>
+          <i title="${category.array[0].name}" class="${category.array[0].icon}"></i>
+          <p>${category.title.toUpperCase()}</p>
+        </div>
+      </button>
     </div>
   `
 })
-const skills = document.querySelector('.skills__section')
-if (skills) skills.innerHTML = skillsHTML
+const skillsMenu = document.querySelector('.skills__menu')
+if (skillsMenu) skillsMenu.innerHTML = menuHTML
 
-// Populates the rows of skills icons
-function handleUI(arr: skillArray[]) {
-    let skillsListHTML = ''
-    arr.forEach(skill => {
-        skillsListHTML+= `
-            <div class="skills__icon hidden__fade">
-                <i title="${skill.name}" class="${skill.icon}"></i>
-                <p>${skill.name}</p>
-            </div>
-        `
-    })
-    return skillsListHTML
-}
-
-// Triggers each category to be rendered when the breakpoint is reached
-window.addEventListener('scroll', () => {
-  const position = Math.floor(scrollY)
-  console.log(position)
-  data.forEach(category => {
-    if (position > category.render.breakpoint && !category.render.flag) {
-      renderSkills(`.skills__${category.title}`)
-      category.render.flag = true
-    }
-  })
+// Adds onclick functionality for the buttons
+data.forEach(category => {
+  const button = document.querySelector(`.skills__menu__${category.title}`)
+  button?.addEventListener('click', () => changePage(category.id))
 })
 
-function renderSkills(category: string) {
-  const iconArray = document.querySelectorAll(`${category} .skills__icon`)
-  let delay = 0
-  iconArray.forEach(icon => {
-      setTimeout(() => {
-          icon.classList.remove('hidden__fade')
-      }, delay)
-      delay+= 150
+function changePage(num: number) {
+  console.log(num)
+  let contentHTML = ''
+  let category = data[num]
+
+  let paragraphHTML = ''
+  category.description.forEach(paragraph => {
+    paragraphHTML += `<p>${paragraph}</p>`
   })
+
+  contentHTML = `
+    <h3>${category.title}</h3>
+    <div>${paragraphHTML}</div>
+    <div class='skills__list'>${handleUI(category.array)}</div>
+  `
+
+  const skills = document.querySelector('.skills__content')
+  if (skills) skills.innerHTML = contentHTML
+
+  function handleUI(arr: skillArray[]) {
+      let skillsListHTML = ''
+      arr.forEach(skill => {
+          skillsListHTML+= `
+              <div class="skills__icon">
+                  <i title="${skill.name}" class="${skill.icon}"></i>
+              </div>
+          `
+      })
+      return skillsListHTML
+  }
 }
+
+changePage(0)
+
+// // Creates the div for each category and populates it, inserts all divs into the top-level 'skills' div
+// let skillsHTML = ''
+// data.forEach(category => {
+//   let descriptionHTML = ''
+//   category.description.forEach(paragraph => {
+//     descriptionHTML+= `<p>${paragraph}</p>`
+//   })
+
+//   skillsHTML+= `
+//     <div class='skills__section'>
+//       <div class='skills__${category.title} skills__list'>
+//         ${handleUI(category.array)}
+//       </div>
+//       <div class='skills__text'>
+//         ${descriptionHTML}
+//       </div>
+//     </div>
+//   `
+// })
+// const skills = document.querySelector('.skills__section')
+// if (skills) skills.innerHTML = skillsHTML
+
+// // Populates the rows of skills icons
+// function handleUI(arr: skillArray[]) {
+//     let skillsListHTML = ''
+//     arr.forEach(skill => {
+//         skillsListHTML+= `
+//             <div class="skills__icon hidden__fade">
+//                 <i title="${skill.name}" class="${skill.icon}"></i>
+//                 <p>${skill.name}</p>
+//             </div>
+//         `
+//     })
+//     return skillsListHTML
+// }
+
+// // Triggers each category to be rendered when the breakpoint is reached
+// window.addEventListener('scroll', () => {
+//   const position = Math.floor(scrollY)
+//   data.forEach(category => {
+//     if (position > category.render.breakpoint && !category.render.flag) {
+//       renderSkills(`.skills__${category.title}`)
+//       category.render.flag = true
+//     }
+//   })
+// })
+
+// function renderSkills(category: string) {
+//   const iconArray = document.querySelectorAll(`${category} .skills__icon`)
+//   let delay = 0
+//   iconArray.forEach(icon => {
+//       setTimeout(() => {
+//           icon.classList.remove('hidden__fade')
+//       }, delay)
+//       delay+= 150
+//   })
+// }
